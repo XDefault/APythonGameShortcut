b5a17c5e3d5df656dee1de7a531a4c40534c8c64
Alexandre Barboza <xansouza100@hotmail.com>
2021-10-06 16:21:12 -0300

    Update ColorsConfig.py

--- Configs/ColorsConfig.py
+++ Configs/ColorsConfig.py
@@ -2,7 +2,7 @@ class COLORS:
     #reds
     RED = (255,0,0)
     MAROON = (128,0,0)
-    DARK_RED = 	(139,0,0)
+    DARK_RED = (139,0,0)
     #black and whites
     WHITE = (255,255,255)
     SILVER = (192,192,192)
@@ -14,11 +14,11 @@ class COLORS:
     #blues
     BLUE = (0,0,255)
     CYAN = (0,255,255)
-    TEAL = 	(0,128,128)
-    NAVY = 	(0,0,128)
+    TEAL = (0,128,128)
+    NAVY = (0,0,128)
     #yellows
     YELLOW = (255,255,0)
     OLIVE = (128,128,0)
     #purples
     MAGENTA = (255,0,255)
-    PURPLE =(128,0,128)
+    PURPLE = (128,0,128)
